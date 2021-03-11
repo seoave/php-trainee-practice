61cc384d0670cafa8ca26a8b02c49c80c8fbe26c
seoave <seo.ave@gmail.com>
2021-03-11 19:07:43 +0200

    add home, registration, login pages

--- index.php
+++ index.php
@@ -13,18 +13,58 @@
 </head>
 <body>
 
-<header class="bg-secondary">
+<header>
     <div class="container">
         <div class="row">
-            <div class="menu">menu</div>
+            <div class="menu border-bottom">
+                <ul class="nav top-menu">
+                    <li class="nav-item menu-item"><a class="nav-link" href="/">Home</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </header>
 
-<main>
+<main class="pt-5 pb-5">
     <div class="container">
         <div class="row">
             <h1>Home</h1>
+        </div>
+        <div class="row post-list">
+            <div class="post-card">
+                <div class="row">
+                    <div class="col image-col">
+                        <figure class="figure">
+                            <img src="assets/images/pic1.jpg" class="figure-img img-fluid" alt="...">
+                            <figcaption class="figure-caption">A caption for the above image.</figcaption>
+                        </figure>
+                    </div>
+                    <div class="col content-col">
+                        <h3 class="post-title">Title</h3>
+                        <div class="post-card-content">
+                            <p>text</p>
+                            <p><a href="#">Read more...</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="post-card">
+                <div class="row">
+                    <div class="col image-col">
+                        <figure class="figure">
+                            <img src="assets/images/pic2.jpg" class="figure-img img-fluid" alt="...">
+                            <figcaption class="figure-caption">A caption for the above image.</figcaption>
+                        </figure>
+                    </div>
+                    <div class="col content-col">
+                        <h3 class="post-title">Title</h3>
+                        <div class="post-card-content">
+                            <p>text</p>
+                            <p><a href="#">Read more...</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </main>
@@ -38,10 +78,6 @@
         </div>
     </div>
 </footer>
-
-
-
-
 
 
 </body>
