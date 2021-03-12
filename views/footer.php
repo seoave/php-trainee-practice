caf15835e0968edc042cae561d137f52d4e147a5
seoave <seo.ave@gmail.com>
2021-03-12 18:47:25 +0200

    add views

---
+++ views/footer.php
@@ -0,0 +1,15 @@
+<?php
+echo '
+<footer class="bg-secondary">
+    <div class="container">
+        <div class="row">
+            <div class="footer-content">
+                <p>2021</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
+';
---
+++ views/header.php
@@ -0,0 +1,28 @@
+<?php
+echo'
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Posts list</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="menu border-bottom">
+                <ul class="nav top-menu">
+                    <li class="nav-item menu-item"><a class="nav-link" href="/">Home</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+';
