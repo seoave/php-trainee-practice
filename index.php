4762feee61ee89c4a7968f0f9f483409d5ff0383
seoave <seo.ave@gmail.com>
2021-03-12 18:45:28 +0200

    add views-layouts

--- index.php
+++ index.php
@@ -1,84 +1,47 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts list</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
+<?php include 'views/header.php'; ?>
 
-<header>
-    <div class="container">
-        <div class="row">
-            <div class="menu border-bottom">
-                <ul class="nav top-menu">
-                    <li class="nav-item menu-item"><a class="nav-link" href="/">Home</a></li>
-                </ul>
+    <main class="pt-5 pb-5">
+        <div class="container">
+            <div class="row">
+                <h1>Home</h1>
             </div>
-        </div>
-    </div>
-</header>
-
-<main class="pt-5 pb-5">
-    <div class="container">
-        <div class="row">
-            <h1>Home</h1>
-        </div>
-        <div class="row post-list">
-            <div class="post-card">
-                <div class="row">
-                    <div class="col image-col">
-                        <figure class="figure">
-                            <img src="assets/images/pic1.jpg" class="figure-img img-fluid" alt="...">
-                            <figcaption class="figure-caption">A caption for the above image.</figcaption>
-                        </figure>
-                    </div>
-                    <div class="col content-col">
-                        <h3 class="post-title">Title</h3>
-                        <div class="post-card-content">
-                            <p>text</p>
-                            <p><a href="#">Read more...</a></p>
+            <div class="row post-list">
+                <div class="post-card">
+                    <div class="row">
+                        <div class="col image-col">
+                            <figure class="figure">
+                                <img src="assets/images/pic1.jpg" class="figure-img img-fluid" alt="...">
+                                <figcaption class="figure-caption">A caption for the above image.</figcaption>
+                            </figure>
+                        </div>
+                        <div class="col content-col">
+                            <h3 class="post-title">Title</h3>
+                            <div class="post-card-content">
+                                <p>text</p>
+                                <p><a href="#">Read more...</a></p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="post-card">
-                <div class="row">
-                    <div class="col image-col">
-                        <figure class="figure">
-                            <img src="assets/images/pic2.jpg" class="figure-img img-fluid" alt="...">
-                            <figcaption class="figure-caption">A caption for the above image.</figcaption>
-                        </figure>
-                    </div>
-                    <div class="col content-col">
-                        <h3 class="post-title">Title</h3>
-                        <div class="post-card-content">
-                            <p>text</p>
-                            <p><a href="#">Read more...</a></p>
+                <div class="post-card">
+                    <div class="row">
+                        <div class="col image-col">
+                            <figure class="figure">
+                                <img src="assets/images/pic2.jpg" class="figure-img img-fluid" alt="...">
+                                <figcaption class="figure-caption">A caption for the above image.</figcaption>
+                            </figure>
+                        </div>
+                        <div class="col content-col">
+                            <h3 class="post-title">Title</h3>
+                            <div class="post-card-content">
+                                <p>text</p>
+                                <p><a href="#">Read more...</a></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 
-<footer class="bg-secondary">
-    <div class="container">
-        <div class="row">
-            <div class="footer-content">
-                <p>2021</p>
-            </div>
-        </div>
-    </div>
-</footer>
-
-
-</body>
-</html>
+<?php include 'views/footer.php';
--- login.php
+++ login.php
@@ -1,62 +1,26 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts list</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body>
+<?php include 'views/header.php'; ?>
 
-<header class="bg-secondary">
-    <div class="container">
-        <div class="row">
-            <div class="menu">menu</div>
-        </div>
-    </div>
-</header>
-
-<main>
-    <div class="container">
-        <div class="row">
-            <div class="form-content">
-                <h1>Login</h1>
-            <form class="userForm" method="post">
-                <div class="mb-3">
-                    <label for="userEmail" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="userEmail" name="userEmail" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="form-content">
+                    <h1>Login</h1>
+                    <form class="userForm" method="post">
+                        <div class="mb-3">
+                            <label for="userEmail" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="userEmail" name="userEmail"
+                                   aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="userPass" name="userPass">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </form>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="userPass" name="userPass">
-                </div>
-                <button type="submit" class="btn btn-primary">Login</button>
-            </form>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 
-<footer class="bg-secondary">
-    <div class="container">
-        <div class="row">
-            <div class="footer-content">
-                <p>2021</p>
-            </div>
-        </div>
-    </div>
-</footer>
-
-
-
-
-
-
-</body>
-</html>
+<?php include 'views/footer.php';
