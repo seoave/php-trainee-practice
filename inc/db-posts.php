682b8a5cf047eb8235439b6b6c8d6e57a7df9ccf
Seoave <seo.ave@gmail.com>
2022-01-16 19:25:52 +0200

    Added posts in loop, posts are in file db. Steps 4-5 done

---
+++ inc/db-posts.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    [
+        'id' => 1,
+        'title' => 'Post title 1',
+        'content' => '<p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be
+                    sure to look under the hood at the source HTML here as we\'ve adjusted the alignment and sizing of
+                    both column\'s content for equal-height.1</p>',
+        'link' => '#post1'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Post title 2',
+        'content' => '<p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be
+                    sure to look under the hood at the source HTML here as we\'ve adjusted the alignment and sizing of
+                    both column\'s content for equal-height.2</p>',
+        'link' => '#post2'
+    ],
+    [
+        'id'=>3,
+        'title'=>'Post title 3',
+        'content'=>'<p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be
+                    sure to look under the hood at the source HTML here as we\'ve adjusted the alignment and sizing of
+                    both column\'s content for equal-height.3</p>',
+        'link'=>'#post3'
+    ],
+    [
+        'id'=>4,
+        'title'=>'Post title 4',
+        'content'=>'<p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be
+                    sure to look under the hood at the source HTML here as we\'ve adjusted the alignment and sizing of
+                    both column\'s content for equal-height.4</p>',
+        'link'=>'#post4'
+    ],
+];
--- index.php
+++ index.php
@@ -1,45 +1,25 @@
 <?php
 
 include 'views/header.php';
-include 'views/layout.php'; ?>
+include 'views/layout.php';
+$posts = require 'inc/db-posts.php';
+$i = 1;
+?>
 <div class="container posts-container">
     <div class="row">
-        <div class="col-md-6 mb-3">
-            <article class="p-5 text-white bg-dark rounded-3">
-                <h3>Post title 1</h3>
-                <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be
-                    sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of
-                    both column's content for equal-height.</p>
-                <button class="btn btn-outline-light">Read more...</button>
-            </article>
-        </div>
-        <div class="col-md-6 mb-3">
-            <article class="p-5 gb-light border rounded-3">
-                <h3>Post title 2</h3>
-                <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be
-                    sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of
-                    both column's content for equal-height.</p>
-                <button class="btn btn-outline-secondary">Read more...</button>
-            </article>
-        </div>
-        <div class="col-md-6 mb-3">
-            <article class="p-5 text-white bg-dark rounded-3">
-                <h3>Post title 1</h3>
-                <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be
-                    sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of
-                    both column's content for equal-height.</p>
-                <button class="btn btn-outline-light">Read more...</button>
-            </article>
-        </div>
-        <div class="col-md-6 mb-3">
-            <article class="p-5 gb-light border rounded-3">
-                <h3>Post title 2</h3>
-                <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be
-                    sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of
-                    both column's content for equal-height.</p>
-                <button class="btn btn-outline-secondary">Read more...</button>
-            </article>
-        </div>
+        <?php foreach ($posts as $post) :
+            $postClasses = ($i % 2) == 0 ? 'gb-light border' : 'text-white bg-dark';
+            $btnClass = ($i % 2) == 0 ? 'btn-outline-secondary' : 'btn-outline-light';
+            ?>
+            <div class="col-md-6 mb-3">
+                <article class="p-5 rounded-3 <?php echo $postClasses; ?>">
+                    <h3><?php echo $post['title']; ?></h3>
+                    <?php echo $post['content']; ?>
+                    <a href="<?php echo $post['link']; ?>" class="btn <?php echo $btnClass; ?>">Read
+                        more...</a>
+            </div>
+        <?php $i++;
+        endforeach;
+        ?>
     </div>
-</div>
-<?php include 'views/footer.php'; ?>
+    <?php include 'views/footer.php'; ?>
